b8a03b6adab98e269e91c4ec958fb3aad77414e5
Ray Bergholm <raybergholm@gmail.com>
2018-10-29 21:31:51 +0200

    add main page body texts

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-const linkUrl = "";
-const linkText = "";
-
 const MainPagePage = () => (
   <div>
-    <p>Page under construction.</p>
-    <p>Check out the main Facebook page<a href={linkUrl}>{linkText}</a> for the latest updates!</p>
   </div>
 );
 
--- src/components/pages/PrivacyPolicyPage.js
+++ src/components/pages/PrivacyPolicyPage.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const PrivacyPolicyPage = () => (
   <div>
-    <p>Lorem ipsum</p>
+    <h2>Our Privacy Policy</h2>
+    <p>Your privacy is important to us.</p>
+    <p>This is why we don't use your private data at all. Whenever you use the Bailemos Helsinki app, it only uses publicly available information posted by the various dance studios, dance teachers, communities and other related event organisers to aggregate the events and courses happening in the Helsinki.</p>
+    <p>Your search terms, your user profiles info and your personal data are never accessed and never saved anywhere because they are not at all necessary for this app to work. In fact, the only reason there's a privacy policy here is because it's required by GDPR and the Facebook Messenger plugin.</p>
   </div>
 );
 
